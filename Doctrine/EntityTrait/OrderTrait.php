c5d06793aca1b79b533433ce69314694e294ed64
Valentin <udaltsov.valentin@gmail.com>
2017-06-21 12:56:07 +0300

    Doctrine / OrderTrait

---
+++ Doctrine/EntityTrait/OrderTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ruwork\CoreBundle\Doctrine\EntityTrait;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+trait OrderTrait
+{
+    /**
+     * @ORM\Column(type="smallint", name="_order")
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type("int")
+     *
+     * @var int
+     */
+    public $order = 0;
+}
