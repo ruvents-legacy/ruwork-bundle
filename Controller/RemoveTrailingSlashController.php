367bd8efd4871f22b54fb38098fc2eca790d71ac
Valentin <udaltsov.valentin@gmail.com>
2017-11-09 15:47:37 +0300

    Удобное подключение RemoveTrailingSlashController-а

--- Controller/RemoveTrailingSlashController.php
+++ Controller/RemoveTrailingSlashController.php
@@ -2,15 +2,11 @@
 
 namespace Ruvents\RuworkBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class RemoveTrailingSlashController
 {
-    /**
-     * @Route("/{path}", requirements={"path": ".*\/$"})
-     */
     public function __invoke(Request $request): RedirectResponse
     {
         $pathInfo = rtrim(urldecode($request->getPathInfo()), ' /');
